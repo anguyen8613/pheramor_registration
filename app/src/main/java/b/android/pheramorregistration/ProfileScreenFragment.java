f25f5f41684c8f9df6093f50682403c7dc4de8a9
Alvin <anguyen8613@gmail.com>
2018-08-02 15:17:12 -0700

    fixed profile page

--- app/src/main/java/b/android/pheramorregistration/ProfileScreenFragment.java
+++ app/src/main/java/b/android/pheramorregistration/ProfileScreenFragment.java
@@ -14,6 +14,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -109,6 +110,8 @@ public class ProfileScreenFragment extends Fragment{
                 }
             }
         });
+
+        Toast.makeText(getActivity(), "Click on Profile Picture", Toast.LENGTH_SHORT).show();
         return v;
     }
 
